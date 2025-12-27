b13f019d77ccbb476e13440b53dc88f518ae28c9
HASSAN SHAHID <mrbeastvenom69@gmail.com>
2025-12-27 05:36:06 +0500

    Created APIs to mark appointment if complete or cancel appointment in Doctor's panel, fetched it in frontend, created the dashboard page for doctor's panel, created api to get dashboard for doc panel, and fetched it.

--- admin/src/Context/doctorContext.jsx
+++ admin/src/Context/doctorContext.jsx
@@ -11,6 +11,7 @@ const doctorContextProvider = (props) => {
 
     const [dToken, setDToken] = useState (localStorage.getItem("dToken") ? localStorage.getItem("dToken") : "")
     const [appointments, setAppointments] = useState([])
+    const [dashData, setDashData] = useState(false)
 
 
     const getAppointments = async () => {
@@ -18,8 +19,8 @@ const doctorContextProvider = (props) => {
             
             const {data} = await axios.get(backendUrl + '/api/doctor/appointments', {headers:{dToken}})
             if (data.success) {
-                setAppointments(data.appointments.reverse())
-                console.log(data.appointments.reverse());
+                setAppointments(data.appointments)
+                console.log(data.appointments);
                 
             } else {
                 toast.error(data.message)
@@ -31,10 +32,69 @@ const doctorContextProvider = (props) => {
         }
     }
 
+    const completeAppointment = async (appointmentId) => {
+
+        try {
+            
+            const {data} = await axios.post(backendUrl + '/api/doctor/complete-appointment', {appointmentId}, {headers:{dToken}})
+            if (data.success) {
+                toast.success(data.message)
+                getAppointments()
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+        console.log(error);
+        toast.error(error.message)
+        }
+
+    }
+
+
+        const cancelAppointment = async (appointmentId) => {
+
+        try {
+            
+            const {data} = await axios.post(backendUrl + '/api/doctor/cancel-appointment', {appointmentId}, {headers:{dToken}})
+            if (data.success) {
+                toast.success(data.message)
+                getAppointments()
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+        console.log(error);
+        toast.error(error.message)
+        }
+
+    }
+
+    const getDashData = async () => {
+        try {
+            
+            const {data} = await axios.get(backendUrl + '/api/doctor/dashboard', {headers:{dToken}})
+            if (data.success) {
+                setDashData(data.dashData)
+                console.log(data.dashData);
+                
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+        console.log(error);
+        toast.error(error.message)
+        }
+    }
+
     const value = {
         dToken, setDToken,
         backendUrl, appointments,
-        setAppointments, getAppointments
+        setAppointments, getAppointments,
+        completeAppointment, cancelAppointment,
+        dashData, setDashData, getDashData
     }
 
     return (
--- backend/Controllers/doctorController.js
+++ backend/Controllers/doctorController.js
@@ -5,7 +5,7 @@ import appointmentModel from "../Models/appointmentModel.js";
 
 const changeAvailability = async (req, res) => {
   try {
-    const  docId  = req.docId;
+    const docId = req.docId;
 
     const docData = await doctorModel.findById(docId);
     await doctorModel.findByIdAndUpdate(docId, {
@@ -56,16 +56,98 @@ const loginDoctor = async (req, res) => {
 // API TO GET DOCTOR APPOINTMENTS FOR DOCTOR PANEL
 const appointmentsDoctor = async (req, res) => {
   try {
+    const docId = req.docId;
+    const appointments = await appointmentModel.find({ docId });
 
-    const docId  = req.docId;
-    const appointments = await appointmentModel.find({ docId })
-
-    res.json({success: true, appointments})
-
+    res.json({ success: true, appointments });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
   }
 };
 
-export { changeAvailability, doctorList, loginDoctor, appointmentsDoctor };
+// API TO MARK APPOINTMENT IS COMPLETE
+const appointmentComplete = async (req,res) => {
+  try {
+    const docId = req.docId;
+    const { appointmentId } = req.body;
+
+    const appointmentData = await appointmentModel.findById(appointmentId);
+
+    if (appointmentData && appointmentData.docId.toString() === docId) {
+      await appointmentModel.findByIdAndUpdate(appointmentId, {
+         doisCompletecId: true,
+      });
+      return res.json({ success: true, message: "Appointment Completed!" });
+    } else {
+      return res.json({ success: false, message: "Mark Failed!" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+
+// API TO CANCEL APPOINTMENT FOR DOC PANEL
+const appointmentCancel = async (req, res) => {
+  try {
+    const docId = req.docId;
+    const { appointmentId } = req.body;
+
+    const appointmentData = await appointmentModel.findById(appointmentId);
+
+    if (appointmentData && appointmentData.docId.toString() === docId) {
+      await appointmentModel.findByIdAndUpdate(appointmentId, {
+        cancelled: true,
+      });
+      return res.json({ success: true, message: "Appointment Cancelled!" });
+    } else {
+      return res.json({ success: false, message: "Cancellation Failed!" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//API TO GET DASHBOARD FOR DOC PANEL
+const doctorDashboard = async(req, res) => {
+  try {
+    
+    const docId = req.docId;
+    const appointments = await appointmentModel.find({docId})
+
+    let earnings = 0
+    
+    appointments.map((item)=>{
+      if (item.doisCompletecId || item.payment) {
+        earnings += item.amount
+      }
+    })
+
+//we will check if in this userId if any data is available in this patients[] array then we will not add that appointment and if data is available then vice versa
+    let patients = []
+
+    appointments.map((item)=> {
+      if (!patients.includes(item.userId)) {
+          patients.push(item.userId)
+      }
+    })
+
+    const dashData = {
+      earnings,
+      appointments: appointments.length,
+      patients: patients.length,
+      latestAppointments: appointments.reverse().slice(0,5)
+    }
+
+    res.json({success:true, dashData})
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+}
+
+export { changeAvailability, doctorList, loginDoctor, appointmentsDoctor, appointmentComplete, appointmentCancel, doctorDashboard };
