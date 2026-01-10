9a2438b1aa060fa5298a8d7ca8b2ed182c713d8a
HASSAN SHAHID <mrbeastvenom69@gmail.com>
2026-01-10 13:10:21 +0500

    Final Modifcations

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../Components/Header'
-import SpecialityMenu from '../Components/specialityMenu'
+import SpecialityMenu from '../Components/SpecialityMenu'
 import TopDoctors from '../Components/TopDoctors'
 import Banner from '../Components/Banner'
 import Footer from '../Components/Footer'
